0dd2f6e3ae29d41a78395dc3b1952c122527a0ef
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2016-05-11 18:17:30 +0200

    #6 Refactor db

--- components/db.js
+++ components/db.js
@@ -1,29 +1,26 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var state = {
-    db: null
+var connection;
+
+module.exports = {
+    connect: connect,
+    get: get
 };
 
-exports.connect = function (url, done) {
-    if (state.db) return done();
-
-    MongoClient.connect(url, function (err, db) {
-        if (err) return done(err);
-        state.db = db;
-        done()
-    })
-};
-
-exports.get = function () {
-    return state.db
-};
-
-exports.close = function (done) {
-    if (state.db) {
-        state.db.close(function (err, result) {
-            state.db = null;
-            state.mode = null;
-            done(err)
-        })
+function connect(url, done) {
+    if (connection) {
+        return done();
     }
-};
+
+    MongoClient.connect(url, function (err, con) {
+        if (err) {
+            return done(err);
+        }
+        connection = con;
+        return done()
+    })
+}
+
+function get() {
+    return connection;
+}
